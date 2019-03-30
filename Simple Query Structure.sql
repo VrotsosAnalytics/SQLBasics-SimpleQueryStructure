79fd84b5799b9cc228ee9006c5adf7de61474083
VrotsosAnalytics <48848953+VrotsosAnalytics@users.noreply.github.com>
2019-03-29 20:57:35 -0700

    Demo Query
    Simple SELECT Statement with various WHERE criteria

---
+++ Simple Query Structure.sql
@@ -0,0 +1,19 @@
+USE Cars
+
+--SELECT
+--	ID
+--	,Name
+
+--FROM
+--	dbo.Owners
+
+--WHERE
+	--ID = 5
+	--ID IN (1,2,3)
+	--ID > 5
+	--Name LIKE '%k%'
+	--Name = 'Julie'
+--	ID IN (SELECT TOP 5 ID FROM dbo.Owners)
+--ORDER BY
+	--ID desc
+	--Name
